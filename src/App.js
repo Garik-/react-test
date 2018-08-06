f042f82df229f16d3021404271fe3cc621dbe0b6
Gar|k <garik.djan@gmail.com>
2018-08-06 15:37:57 +0500

    Эта хрень работает, но пока всё не красиво

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import {
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+
+
+
+import './App.css';
+
+const items = [
+      {
+        'id': 1,
+        'name': 'Home',
+        'href': '/'
+      },
+      {
+        'id': 2,
+        'name': 'About Us',
+        'href': '/about'
+      },
+      {
+        'id':3,
+        'name': 'Contact Us',
+        'href': '/contacts'
+      }
+    ];
+
+const Header = () => (
+  <header>
+    <ul className="navigation">
+      {items.map(item => 
+        <li key={item.id} className="navigation__item">
+          <Link className="navigation__item--link" to={item.href}>{ item.name }</Link>
+        </li>)}
+    </ul>
+  </header>
+      )
+
+
+const About = () => (
+  <div>About</div>
+)
+
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Tornadoes Website!</h1>
+  </div>
+)
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/about' component={About}/>
+    </Switch>
+  </main>
+)
+
+class App extends Component {
+  render() {
+    return ( 
+      <div><Header/><Main /></div>
+    );
+  }
+}
+
+export default App;
